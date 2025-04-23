83dae15484b66f5040c5a6b2f4dc7fe9cfb1dfc4
Tim Solfronk <tim.solfronk@gmail.com>
2025-04-23 19:54:31 +0200

    First prototype implemented

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float accelerationFactor = 1;
+    private float accelerationFactor = 6;
     [SerializeField]
     private float slowingFactor = 2;
     [SerializeField]
@@ -12,21 +13,31 @@ public class PlayerMovement : MonoBehaviour
 
 
     private float currentSpeed = 0;
-    private Vector2 moveDir = new Vector2(0, 1);
+    private Vector2 moveInput = new Vector2(0, 0);
     private Rigidbody rb;
+    private Transform playerCamera;
+
+    private Vector2 mouseMovement;
+
+    private float mouseSensitivity = 1f;
+    private float rotationX;
+    private float rotationY;
+    private float lookXLimit = 90;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        playerCamera = GetComponentInChildren<Camera>().transform;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         UpdateMovementSpeed();
-        UpdateRotation();
         SetHorizontalVelocity();
     }
 
@@ -34,17 +45,42 @@ public class PlayerMovement : MonoBehaviour
     {
         if (currentSpeed < maxSpeed)
         {
-            currentSpeed += accelerationFactor * 0.1f;
+            currentSpeed += accelerationFactor * 0.2f;
         }
     }
 
     private void UpdateRotation()
     {
-        
+        rotationX += -mouseMovement.y * 0.05f * mouseSensitivity;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+
+        rotationY += mouseMovement.x * 0.1f * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(0, rotationY, 0);
     }
 
     private void SetHorizontalVelocity ()
     {
-        rb.linearVelocity = new Vector3(moveDir.x * currentSpeed, rb.linearVelocity.y, moveDir.y * currentSpeed);
+        Vector3 moveDir = currentSpeed * (transform.forward * moveInput.y + transform.right * moveInput.x) + new Vector3(0, rb.linearVelocity.y,0);
+
+        rb.linearVelocity = moveDir;
     }
+
+    public void MouseMoved(InputAction.CallbackContext context)
+    {
+        mouseMovement = context.ReadValue<Vector2>();
+        UpdateRotation();
+    }
+
+    public void Moving(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>().normalized;
+        if(moveInput.x == 0 && moveInput.y == 0)
+        {
+            currentSpeed = maxSpeed/10f;
+        }
+    }
+
 }
+
